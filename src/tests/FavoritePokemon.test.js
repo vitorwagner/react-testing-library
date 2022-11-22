02589cf8384c448acbf67204354aef4db73939f0
Vitor Wagner <vitormwagner@gmail.com>
2022-11-22 15:25:25 -0300

    Adiciona teste para componente FavoritePokemon no caso de nenhum pokemon favorito

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('3. Testa o componente <FavoritePokemon.js />', () => {
+  test('Testa se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const noFavoritesMessage = screen.getByText(/no favorite pokémon found/i);
+
+    expect(noFavoritesMessage).toBeInTheDocument();
+  });
+});
